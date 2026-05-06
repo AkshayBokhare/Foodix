70eeb7f60c55cebb86b70b4317780ead9f706a31
NTS-AkshayBokhare <akshay.bokhare@neutrinotechlabs.com>
2026-05-06 17:22:03 +0530

    used useState()

--- src/componets/BodyComponent.jsx
+++ src/componets/BodyComponent.jsx
@@ -1,25 +1,55 @@
 import "../styles/body_component.css"
 import CardItems from "./CardItems";
 import { restaurants as data } from "../utils/data.js";
+import { useState } from "react";
 
 const BodyComponent = () => {
+    //State variable
+    const [restoList, setRestoList] = useState(data);
+    const [filterRestoList, setfilterRestoListCount] = useState(data.length);
+    const [count, setCount] = useState(0);
+
     return (
         <div className="body">
             <div className="search-bar">
                 Search Bar
             </div>
+            <button className="filter-btn"
+                onClick={() => {
+
+                    const filterRestoList = restoList.filter(res => res.data.rating > 4);
+                    console.log(restoList.length);
+                    console.log(filterRestoList);
+
+                    console.log(filterRestoList.length);
+                    setfilterRestoListCount(filterRestoList.length);
+                    setRestoList(filterRestoList);
+                }}>
+                Click to See Top Rated Resto {filterRestoList}
+            </button>
 
             <div className="food-items-card">
-                {data.map((restaurant) => (
+                {restoList.map((restaurant) => (
+                    console.log("Restaurants in list: " + restoList.length),
                     <CardItems
-                       key={restaurant.data.id}
-                       restaurant={restaurant}
+                        key={restaurant.data.id}
+                        restaurant={restaurant}
                     />
+
                 ))}
 
             </div>
 
+            <div className="counter">
+                <h1> Your Count Is: {count}</h1>
+                <button className="counter-btn" onClick={
+                    () => setCount(count + 1)       
+                }>
+                    Click to See Count
+                </button>
+            </div>
         </div>
+       
     );
 }
 
--- src/utils/data.js
+++ src/utils/data.js
@@ -5,7 +5,7 @@ export const restaurants = [
             imgurl:"https://b.zmtcdn.com/data/pictures/0/20866140/d71443c786caaf06694b50a699a8a5d6_featured_v2.jpg",
             name: "McDonald's",
             cuisine: "Burgers, Fast Food",
-            rating: "4.5 ⭐",
+            rating: 5,
             price: "₹ 300 for one"
         }
     },
@@ -15,7 +15,7 @@ export const restaurants = [
             imgurl:"https://tb-static.uber.com/prod/image-proc/processed_images/a7c701cd77728f6ded280d8d948db4e4/5283d81c664b43c5f57a3a186d273063.jpeg",
             name: "Domino's Pizza",
             cuisine: "Pizza, Italian",
-            rating: "4.3 ⭐",
+            rating: 4.2,
             price: "₹ 400 for one"
         }
     },
@@ -25,7 +25,7 @@ export const restaurants = [
             imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJuR6mOoDAtz6QiDum9dk5Rlt2YoD_FRfgLg&s",
             name: "Haldiram's",
             cuisine: "Indian, Snacks, Sweets",
-            rating: "4.6 ⭐",
+            rating:3.8,
             price: "₹ 250 for one"
         }
     },
@@ -35,7 +35,7 @@ export const restaurants = [
             imgurl: "https://a.mktgcdn.com/p/1am3axZ4_GOlZkUD4Fav-3tque8ZLg5HYehnmIGjDHA/1050x600.png",
             name: "Subway",
             cuisine: "Sandwiches, Healthy Food",
-            rating: "4.2 ⭐",
+            rating: 4.7,
             price: "₹ 350 for one"
         }
     },
@@ -45,7 +45,7 @@ export const restaurants = [
             imgurl: "https://www.drsdeals.in/wp-content/uploads/2022/04/Barbeque-Nationfbd54cc762ea3de8f.webp",
             name: "Barbeque Nation",
             cuisine: "BBQ, North Indian, Continental",
-            rating: "4.7 ⭐",
+            rating: 4.7,
             price: "₹ 1200 for one"
         }
     },
@@ -55,7 +55,7 @@ export const restaurants = [
             imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ04Y6ihnGl_gTWrvie6dWfSylNFGMEwCcRew&s",
             name: "Burger King",
             cuisine: "Burgers, Fast Food",
-            rating: "4.1 ⭐",
+            rating: 3.1,
             price: "₹ 280 for one"
         }
     },
@@ -65,17 +65,17 @@ export const restaurants = [
             imgurl: "https://b.zmtcdn.com/data/brand_creatives/logos/116fd4c89f628d110a526c2fdea08b16_1726681936.png?crop=700%3A400%3B%2A%2C%2A&fit=around%7C700%3A400&output-format=webp",
             name: "Paradise Biryani",
             cuisine: "Biryani, Hyderabadi",
-            rating: "4.9 ⭐",
+            rating: 4.4,
             price: "₹ 450 for one"
         }
     },
     {
         data: {
-            id: "7",
+            id: "8",
             imgurl: "https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/inline-images/Wow%21%20Momo.jpg",
             name: "WoW MOMOs",
             cuisine: "Momos",
-            rating: "4.9 ⭐",
+            rating: 4,
             price: "₹ 250 for one"
         }
     }
