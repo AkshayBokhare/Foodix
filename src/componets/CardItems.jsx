cf8971a1b20b1363d98ca4f1ec91516bd41e1e67
NTS-AkshayBokhare <akshay.bokhare@neutrinotechlabs.com>
2026-05-06 14:04:47 +0530

    add imgURL

--- src/componets/CardItems.jsx
+++ src/componets/CardItems.jsx
@@ -1,13 +1,12 @@
 import "../styles/carditems.css";
-import biryaniImg from "../assets/biryani.jpg";
 
 const CardItems = ({ restaurant }) => {
     return (
 
         <div className="res-card">
 
-            <img src={biryaniImg} alt="Food Image" />
-            <h3>{restaurant.data.name}</h3> 
+            <img src={restaurant.data.imgurl} alt="Food Image" />
+            <h3>{restaurant.data.name}</h3>
             <p>{restaurant.data.cuisine}</p>
             <p>{restaurant.data.rating}</p>
             <p>{restaurant.data.price}</p>
